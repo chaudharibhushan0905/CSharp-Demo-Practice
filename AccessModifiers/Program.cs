461c9b2207bb1ad7d502fe6994d1da596380d6cc
chaud <chaud@BHUSHAN>
2022-09-21 11:42:02 +0530

    Class Object

---
+++ AccessModifiers/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AccessModifiers
+{
+    class Person
+    {
+        public Person()
+        {
+
+        }
+        private int id;
+
+        public string Name { get; set; }
+
+        protected string Address { get; set; }
+    }
+
+    class TestPerson : Person
+    {
+        public void Print()
+        {
+            Address = "India";
+        }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Person person = new Person();
+
+            TestPerson testPerson = new TestPerson();
+            Console.ReadLine();
+
+
+        }
+    }
+
+}
---
+++ ClassObj/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassObject
+{
+
+    /* class Car
+     {
+         string color = "black";
+
+         static void Main(string[] args)
+         {
+             Car c = new Car();
+             Console.WriteLine(c.color);
+             Console.ReadLine();
+         }
+     }*/
+    public class Student
+    {
+        int id;
+        string name;
+
+        public static void Main(string[] args)
+        {
+            Student s = new Student();
+            s.id = 47;
+            s.name = "Bhushan";
+            Console.WriteLine(s.id);
+            Console.WriteLine(s.name);
+            Console.ReadLine();
+        }
+    }
+}
